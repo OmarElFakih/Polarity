f1e73a61926389f53507cee0ad9c865c7bc570be
OmarElFakih <omarelfakih97@gmail.com>
2019-07-11 13:26:08 -0400

    polarity base created
    the base classes for the polarity mechanics were created but are yet to be tested. SFX need fixing

---
+++ Polarity/Assets/_Scripts/ShiftingPolarityBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShiftingPolarityBehaviour : PolarityBehaviour
+{
+    [SerializeField]
+    float lerp_t = 0f;
+
+    private Color _targetColor = Color.white;
+
+    protected override void Start()
+    {
+        base.Start();
+        _targetColor = _renderer.color;
+    }
+
+    private void Update()
+    {
+        _renderer.color = Color.Lerp(_renderer.color, _targetColor, lerp_t * Time.deltaTime);
+    }
+
+
+    public void ShiftPolarity(Polarity pol)
+    {
+        _polarity = pol;
+
+        _targetColor = (_polarity == Polarity.On) ? _onColor : _offColor;
+    }
+
+
+
+}
---
+++ Polarity/Assets/_Scripts/Token.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Token : PolarityBehaviour
+{
+    [SerializeField]
+    private GameObject _beat = null;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        PolarityBehaviour contact = other.GetComponent<PolarityBehaviour>();
+
+        if (contact.GetPolarity() == this._polarity)
+        {
+            //score
+            Instantiate(_beat, Vector3.zero, Quaternion.identity);
+            Debug.Log("Score");
+        }
+        else
+        {
+            //hurt
+            Debug.Log("Hurt");
+        }
+    }
+
+}
